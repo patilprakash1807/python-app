09cdd492a144fe7605529ff39838c58310423349
prakash-patil1807 <patilprakash1807@gmail.com>
2025-10-24 00:26:36 +0530

    Adds short form for commits

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         'hostname': socket.gethostname(),
-        'message': 'Hello, World!'
+        'message': 'Hello, Praks!'
     })
 
 @app.route('/api/v1/healthz')
