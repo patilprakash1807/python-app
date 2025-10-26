f6f96678ef7593ec0952e94ea062978f0476d704
prakash-patil1807 <patilprakash1807@gmail.com>
2025-10-26 13:22:39 +0530

    Modifies values file on CD step

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         'hostname': socket.gethostname(),
-        'message': 'Hello, Dash!!!!!'
+        'message': 'Hello, Dash! <3'
     })
 
 @app.route('/api/v1/healthz')
